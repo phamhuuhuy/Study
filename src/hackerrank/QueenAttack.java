cc9e8be2656acde82a9aaed8502a145df9b01d35
phamhuuhuy <huykiengabc9@gmail.com>
2024-05-08 14:43:47 +0700

    new commit

--- src/hackerrank/QueenAttack.java
+++ src/hackerrank/QueenAttack.java
@@ -1,8 +1,6 @@
 package hackerrank;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class QueenAttack {
     public static int queensAttack(int n, int k, int r_q, int c_q, List<List<Integer>> obstacles) {
@@ -64,6 +62,86 @@ public class QueenAttack {
         return result;
     }
 
+    public static int queensAttack1(int n, int k, int r_q, int c_q, List<List<Integer>> obstacles) {
+        // Write your code here
+        HashMap<Integer, HashSet<Integer>> cache = new HashMap<>();
+        for (int i = 0; i < obstacles.size(); i++) {
+            if (cache.containsKey(obstacles.get(i).get(0))) {
+                cache.get(obstacles.get(i).get(0)).add(obstacles.get(i).get(1));
+            } else {
+                cache.put(obstacles.get(i).get(0), new HashSet<Integer>());
+                cache.get(obstacles.get(i).get(0)).add(obstacles.get(i).get(1));
+            }
+        }
+        int counter = 0;
+        // right
+        for (int i = c_q + 1; i <= n; i++) {
+            if (cache.containsKey(r_q) && cache.get(r_q).contains(i)) {
+                break;
+            }
+            counter++;
+        }
+
+        // left
+        for (int i = c_q - 1; i >= 1; i--) {
+            if (cache.containsKey(r_q) && cache.get(r_q).contains(i)) {
+                break;
+            }
+            counter++;
+        }
+
+        // down
+        for (int i = r_q + 1; i <= n; i++) {
+            if (cache.containsKey(i) && cache.get(i).contains(c_q)) {
+                break;
+            }
+            counter++;
+        }
+
+        // up
+        for (int i = r_q - 1; i >= 1; i--) {
+            if (cache.containsKey(i) && cache.get(i).contains(c_q)) {
+                break;
+            }
+            counter++;
+        }
+
+        // up-left
+        for (int i = r_q - 1, j = c_q - 1; i >= 1 && j >= 1; i--, j--) {
+            if (cache.containsKey(i) && cache.get(i).contains(j)) {
+                break;
+            }
+            counter++;
+        }
+
+        // up-right
+        for (int i = r_q - 1, j = c_q + 1; i >= 1 && j <= n; i--, j++) {
+            if (cache.containsKey(i) && cache.get(i).contains(j)) {
+                break;
+            }
+            counter++;
+        }
+
+        // down-right
+        for (int i = r_q + 1, j = c_q + 1; i <= n && j <= n; i++, j++) {
+            if (cache.containsKey(i) && cache.get(i).contains(j)) {
+                break;
+            }
+            counter++;
+        }
+
+        // down-left
+        for (int i = r_q + 1, j = c_q - 1; i <= n && j >= 1; i++, j--) {
+            if (cache.containsKey(i) && cache.get(i).contains(j)) {
+                break;
+            }
+            counter++;
+        }
+
+        return counter;
+
+    }
+
     public static void main(String[] args) {
         List<List<Integer>> ob = new ArrayList<>();
         List<Integer> integers1 = new ArrayList<>(Arrays.asList(4,2));
@@ -72,4 +150,6 @@ public class QueenAttack {
         ob.add(integers2);
         System.out.println(queensAttack(5,4, 4,3, ob));
     }
+
 }
+
